6b2f0496b377a9aa32f53c467623947d2f4ca208
vmatveev <vmatveevspam@gmail.com>
2021-07-01 19:15:25 -0400

    3.3 done

--- server.js
+++ server.js
@@ -35,6 +35,17 @@ app.get('/api/persons', (req, res) => {
     res.json(persons);
 });
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
 app.get('/info', (req, res) => {
     let numberOfEntries = persons.length;
     let date = new Date()
