8491c17237c9a56845389b172670ee0283d5bc66
vmatveev <vmatveevspam@gmail.com>
2021-07-01 19:21:09 -0400

    3.4 done

--- server.js
+++ server.js
@@ -55,6 +55,13 @@ app.get('/info', (req, res) => {
     `)
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`);
 })
