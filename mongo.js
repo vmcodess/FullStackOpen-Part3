3fab8cfe266fb276f75a0804aa5d7d726bbccd14
vmatveev <vmatveevspam@gmail.com>
2021-07-09 23:41:05 -0400

    3.12 done

---
+++ mongo.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>');
+    process.exit(1);
+};
+
+//const password = process.argv[2];
+const url = 
+`mongodb+srv://fullstack:${process.env.MONGO_DB_PASSWORD}@cluster0.geiqr.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+.then(() => {
+    console.log('Connected to DB!')
+})
+.catch((err) => {
+    console.log(`error connecting to db: ${err}`)
+})
+
+const phonebookSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Users', phonebookSchema)
+
+// const user = new Phonebook({
+//     name: process.argv[3],
+//     number: process.argv[4],
+//     date: new Date()
+// })
+
+if (process.argv.length[3]) {
+    const name = process.argv[3]
+    const number = process.argv[4]
+    const person = new Person({
+        name: name,
+        number: number
+    })
+
+    Person.save().then(result => {
+        console.log(`added ${user.name} number ${user.number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    Person.find({}).then(result => {
+        console.log('phonebook')
+        result.forEach(person => {
+            let p = `${person.name} ${person.number}`
+            console.log(p)
+        })
+        mongoose.connection.close()
+    })
+}
+
+
+
+
+
+
+
+module.exports = Person;
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
-const morgan = require('morgan')
-const cors = require('cors')
-const PORT = process.env.PORT || 3001
+const express = require('express');
+const app = express();
+app.use(express.json());
+const morgan = require('morgan');
+const cors = require('cors');
+const Phonebook = require('./mongo')
+require('dotenv').config()
+const PORT = process.env.PORT || 3001;
 
-app.use(cors())
-app.use(express.static('build')) // To serve static files such as images, CSS files, and JavaScript files.
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(cors());
+app.use(express.static('build')); // To serve static files such as images, CSS files, and JavaScript files.
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
 
 morgan.token('data', (req, res) => {
     if (req.method === 'POST') {
@@ -16,7 +18,7 @@ morgan.token('data', (req, res) => {
     else {
         return ''
     }
-})
+});
 
 let persons = 
 [
@@ -49,7 +51,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons);
+    res.json(users)
 });
 
 app.get('/api/persons/:id', (req, res) => {
@@ -61,31 +63,31 @@ app.get('/api/persons/:id', (req, res) => {
     } else {
         res.status(404).end()
     }
-})
+});
 
 app.get('/info', (req, res) => {
     let numberOfEntries = persons.length;
-    let date = new Date()
+    let date = new Date();
     res.send(`
         Phonebook has info for ${numberOfEntries} people <br><br>
         ${date}
-    `)
-})
+    `);
+});
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
+    const id = Number(req.params.id);
+    persons = persons.filter(person => person.id !== id);
 
-    res.status(204).end()
-})
+    res.status(204).end();
+});
 
 const generateId = () => {
-    return Math.floor(Math.random() * 100)
-} 
+    return Math.floor(Math.random() * 100);
+};
 
 app.post('/api/persons', (req, res) => {
-   const person = req.body
-    const nameExists = persons.find(p => p.name === person.name)
+    const person = req.body;
+    const nameExists = persons.find(p => p.name === person.name);
 
     if (!person.name) {
         return res.status(404).json({
@@ -98,15 +100,15 @@ app.post('/api/persons', (req, res) => {
     } else if (!person.number) {
         return res.status(404).json({
             error: 'number is missing'
-        })
-    }
+        });
+    };
 
-    person.id = generateId()
-    persons = persons.concat(person)
-    res.json(persons)
-})
+    person.id = generateId();
+    persons = persons.concat(person);
+    res.json(persons);
+});
 
 // deployed to heroku : success
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-})
+});
