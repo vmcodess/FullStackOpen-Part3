fcfb31e2d01fe55a4e539b3ca9d5c26d7861864d
vmatveev <vmatveevspam@gmail.com>
2021-07-10 16:19:59 -0400

    3.13 - 3.14

---
+++ models/phonebook.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+
+const url = process.env.MONGODB_URI;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+.then(() => {
+    console.log('Connected to DB!')
+})
+.catch((err) => {
+    console.log(`error connecting to db: ${err}`)
+})
+
+const phonebookSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+phonebookSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+})
+
+//const Person = mongoose.model('Users', phonebookSchema)
+
+module.exports = mongoose.model('Users', phonebookSchema)
+
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
 const morgan = require('morgan');
 const cors = require('cors');
-const Phonebook = require('./mongo')
-require('dotenv').config()
+//const mongoose = require('mongoose');
+const Person = require('./models/phonebook');
+require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
 app.use(cors());
 app.use(express.static('build')); // To serve static files such as images, CSS files, and JavaScript files.
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
@@ -20,49 +21,47 @@ morgan.token('data', (req, res) => {
     }
 });
 
-let persons = 
-[
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+let persons = [
+    {
+      id: 1,
+      name: "Arto Hellas",
+      number: "040-123456"
     },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+      id: 2,
+      name: "Ada Lovelace",
+      number: "39-44-5323523"
     },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+      id: 3,
+      name: "Dan Abramov",
+      number: "12-43-234345"
     },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+      id: 4,
+      name: "Mary Poppendieck",
+      number: "39-23-6423122"
     }
-];
-
-
+  ]
 
 app.get('/', (req, res) => {
     res.send('Phonebook Backend');
 });
 
 app.get('/api/persons', (req, res) => {
-    res.json(users)
+    Person.find({}).then(data => {
+        res.json(data)
+    })
 });
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {
+    Person.findById(req.params.id)
+    .then(person => {
         res.json(person)
-    } else {
-        res.status(404).end()
-    }
+    })
+    .catch((err) => {
+        console.log(`Error finding person by id: ${err}`);
+    })
 });
 
 app.get('/info', (req, res) => {
@@ -81,34 +80,30 @@ app.delete('/api/persons/:id', (req, res) => {
     res.status(204).end();
 });
 
-const generateId = () => {
-    return Math.floor(Math.random() * 100);
-};
 
 app.post('/api/persons', (req, res) => {
-    const person = req.body;
-    const nameExists = persons.find(p => p.name === person.name);
+    const body = req.body;
 
-    if (!person.name) {
+    if (!body.name) {
         return res.status(404).json({
             error: 'name is missing'
         })
-    } else if (nameExists) {
-        return res.status(400).json({
-            error: 'name already exists'
-        })
-    } else if (!person.number) {
+    } else if (!body.number) {
         return res.status(404).json({
             error: 'number is missing'
         });
     };
 
-    person.id = generateId();
-    persons = persons.concat(person);
-    res.json(persons);
+    const person = new Person({
+        name: body.name,
+        number: body.number,
+    })
+
+    person.save().then(savedPerson => {
+        res.json(savedPerson.toJSON())
+    })
 });
 
-// deployed to heroku : success
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
